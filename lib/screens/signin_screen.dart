98879b172c3b7fbc5246e13a95da99ee999fd8fc
lazy-david <davidiwuji@hotmail.com>
2023-01-13 00:05:45 +0100

    finished the login area

--- lib/screens/signin_screen.dart
+++ lib/screens/signin_screen.dart
@@ -1,6 +1,8 @@
 import 'dart:ui';
 
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:nnnnnnnnnn/screens/home_screen.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
@@ -10,8 +12,30 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  static Future<User?> loginUsingEmailPassword(
+      {required String email,
+      required String password,
+      required BuildContext context}) async {
+    FirebaseAuth auth = FirebaseAuth.instance;
+    User? user;
+    try {
+      UserCredential userCredential = await auth.signInWithEmailAndPassword(
+          email: email, password: password);
+      user = userCredential.user;
+    } on FirebaseAuthException catch (e) {
+      if (e.code == "user-not-found") {
+        print("No User found for that email");
+      }
+    }
+
+    return user;
+  }
+
   @override
   Widget build(BuildContext context) {
+    // create the textfield controller
+    TextEditingController _emailController = TextEditingController();
+    TextEditingController _passwordController = TextEditingController();
     return Padding(
       padding: EdgeInsets.all(16.0),
       child: Column(
@@ -35,6 +59,7 @@ class _LoginScreenState extends State<LoginScreen> {
             height: 44,
           ),
           TextField(
+            controller: _emailController,
             keyboardType: TextInputType.emailAddress,
             decoration: InputDecoration(
                 hintText: "User Email",
@@ -47,6 +72,7 @@ class _LoginScreenState extends State<LoginScreen> {
             height: 26.0,
           ),
           TextField(
+            controller: _passwordController,
             obscureText: true,
             decoration: InputDecoration(
                 hintText: "User Password",
@@ -73,7 +99,20 @@ class _LoginScreenState extends State<LoginScreen> {
               padding: EdgeInsets.symmetric(vertical: 20),
               shape: RoundedRectangleBorder(
                   borderRadius: BorderRadius.circular(12)),
-              onPressed: () {},
+              onPressed: () async {
+                // test
+                User? user = await loginUsingEmailPassword(
+                        email: _emailController.text,
+                        password: _passwordController.text,
+                        context: context),
+                    print;
+                (user);
+                if (user != null) {
+                  Navigator.of(context).pushReplacement(
+                      MaterialPageRoute(builder: (context) => ProfileScreen()));
+                  // lets make a new screen
+                }
+              },
               child: Text(
                 "Login",
                 style: TextStyle(
