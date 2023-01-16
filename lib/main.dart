a433241a803008f8cdf03a5429617180603b7f0f
lazy-david <davidiwuji@hotmail.com>
2023-01-16 16:50:33 +0100

    reedit

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,9 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:nnnnnnnnnn/resuable_widget/resucable_widget.dart';
+import 'package:nnnnnnnnnn/mis/auth_page.dart';
 import 'package:nnnnnnnnnn/screens/signin_screen.dart';
 
-Future<void> main() async {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   runApp(const MyApp());
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: MainPage(),
+      home: LoginScreen(),
     );
   }
 }
--- lib/screens/signin_screen.dart
+++ lib/screens/signin_screen.dart
@@ -1,141 +1,109 @@
 import 'dart:ui';
 
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:nnnnnnnnnn/screens/home_screen.dart';
 
-class LoginScreen extends StatefulWidget {
+class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
 
   @override
-  State<LoginScreen> createState() => _LoginScreenState();
-}
-
-class _LoginScreenState extends State<LoginScreen> {
-  // required so we can controll whats in the text feild
-  final _emailController = TextEditingController();
-  final _passwordController = TextEditingController();
-
-  Future signIn() async {
-    // adding Firebase sign in method for the login
-    await FirebaseAuth.instance.signInWithEmailAndPassword(
-        email: _emailController.text.trim(),
-        password: _passwordController.text.trim());
-  }
-
-  // for the memory of the app
-  @override
-  void dispose() {
-    _emailController.dispose();
-    _passwordController.dispose();
-  }
-
-  @override
   Widget build(BuildContext context) {
-    // create the textfield controller
-    TextEditingController _emailController = TextEditingController();
-    TextEditingController _passwordController = TextEditingController();
-    return Padding(
-      padding: EdgeInsets.all(16.0),
-      child: Center(
+    return Scaffold(
+      backgroundColor: Colors.grey[100],
+      body: Center(
+        // Single Child Scrool view
         child: SingleChildScrollView(
+          // begin of first column
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Text(
-                "My App Title",
-                style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 28,
-                    fontWeight: FontWeight.bold),
+              Icon(
+                Icons.anchor_rounded,
+                size: 100,
               ),
               Text(
-                "Login to your App",
-                style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 44.0,
-                  fontWeight: FontWeight.bold,
-                ),
+                "Welcome back, you've been missed",
+                style: TextStyle(fontSize: 16),
               ),
+
               SizedBox(
-                height: 44,
+                height: 70,
               ),
+              // Email TextField
               Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 0),
+                padding: EdgeInsets.symmetric(horizontal: 25),
                 child: TextField(
-                  controller: _emailController,
-                  keyboardType: TextInputType.emailAddress,
                   decoration: InputDecoration(
-                      hintText: "Email",
-                      prefixIcon: Icon(
-                        Icons.mail,
-                        color: Colors.black,
-                      ),
-                      enabledBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(12),
-                        borderSide: BorderSide(color: Colors.black),
-                      ),
-                      focusedBorder: OutlineInputBorder(
-                        borderSide: BorderSide(color: Colors.deepPurple),
-                        borderRadius: BorderRadius.circular(12),
-                      )),
-                ),
-              ),
-              SizedBox(
-                height: 12.0,
-              ),
-              TextField(
-                controller: _passwordController,
-                obscureText: true,
-                decoration: InputDecoration(
-                    hintText: "User Password",
-                    prefixIcon: Icon(
-                      Icons.lock,
-                      color: Colors.black,
-                    ),
                     enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.white),
                       borderRadius: BorderRadius.circular(12),
-                      borderSide: BorderSide(color: Colors.black),
                     ),
                     focusedBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.deepPurple),
+                      borderSide: BorderSide(color: Colors.white),
                       borderRadius: BorderRadius.circular(12),
-                    )),
+                    ),
+                    hintText: "Email",
+                    fillColor: Colors.grey[200],
+                    filled: true,
+                    prefixIcon: Icon(
+                      Icons.mail_outline_rounded,
+                      color: Colors.orange,
+                    ),
+                  ),
+                ),
               ),
+
               SizedBox(
-                height: 12,
+                height: 10,
               ),
-              Text(
-                "Don't remember your password?",
-                style: TextStyle(color: Colors.blue),
+              // Password TextField
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 25),
+                child: TextField(
+                  decoration: InputDecoration(
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.white),
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.white),
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    hintText: "Password",
+                    fillColor: Colors.grey[200],
+                    filled: true,
+                    prefixIcon: Icon(
+                      Icons.lock_outline_rounded,
+                      color: Colors.orange,
+                    ),
+                  ),
+                ),
               ),
+
               SizedBox(
-                height: 88,
+                height: 50,
               ),
-              Container(
-                width: double.infinity,
-                child: RawMaterialButton(
-                  fillColor: Color(0xFF0069FE),
-                  elevation: 0.0,
-                  padding: EdgeInsets.symmetric(vertical: 20),
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(12)),
-                  onPressed: () async {
-                    // calling the signIn() function above
-                    signIn();
-                  },
-                  child: Text(
-                    "Login",
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 18,
+              // Button
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 25),
+                child: Container(
+                  padding: EdgeInsets.all(12),
+                  decoration: BoxDecoration(
+                    color: Colors.orange,
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  child: Center(
+                    child: Text(
+                      "Sign In",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18),
                     ),
                   ),
                 ),
               )
             ],
           ),
+          // ending of first column
         ),
       ),
     );
